fe5c3ce9e176c46c09f7ad8621b7922ccedf5070
tarafdar16 <imrantarafdar16@gmail.com>
2024-02-04 11:58:18 +0600

    done

---
+++ Components/TopicsList.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
+
+const getTopics = async () => {
+  try {
+    const res = await fetch("https://mongodbcrud-ep2bb3s57-mostafizur64.vercel.app/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+    return { topics: [] }; // Return an empty array in case of error
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
+  if (!topics) {
+    return <div>Loading...</div>; // Handle loading state
+  }
+
+  return (
+    <>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
